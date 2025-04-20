de62e75d50d2d49af0d18d5e23dbe05e6dd7f764
Diego Ongaro <ongardie@gmail.com>
2025-04-19 18:19:12 -0700

    Add jj-related configs

--- cubicle/configs/.config/profile.d/20-configs-vars.sh
+++ cubicle/configs/.config/profile.d/20-configs-vars.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-export LC_COLLATE='C'
+export EDITOR=vim
+export LC_COLLATE=C
 export PAGER='less --chop-long-lines --jump-target=5 --ignore-case --no-init --RAW-CONTROL-CHARS --quit-if-one-screen --shift=.2'
